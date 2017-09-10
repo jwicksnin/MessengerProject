6e937e1b8c70b74af069c9dc1ea12d0f6d5c0494
Jessica Wicksnin <jwicksnin@gmail.com>
2017-09-10 16:31:55 -0700

    Wrap the call to the send API in a Promise.

--- bot.js
+++ bot.js
@@ -182,25 +182,29 @@ function sendGenericMessage(recipientId) {
 }
 
 function callSendAPI(messageData) {
-  request({
+  return new Promise((resolve, reject) => {
+    request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
     method: 'POST',
     json: messageData
 
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      const recipientId = body.recipient_id;
-      const messageId = body.message_id;
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        const recipientId = body.recipient_id;
+        const messageId = body.message_id;
 
-      console.log("Successfully sent generic message with id %s to recipient %s", 
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });  
+        console.log("Successfully sent generic message with id %s to recipient %s",
+          messageId, recipientId);
+        resolve(body);
+      } else {
+        console.error("Unable to send message.");
+        console.error(response);
+        console.error(error);
+        reject(error);
+      }
+    });
+  });
 }
 
 // Set Express to listen out for HTTP requests
